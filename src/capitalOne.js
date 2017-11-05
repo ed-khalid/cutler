90289ee85ffff5bef1f8b5f781e19528c6f25022
ed-khalid <ed.khalid1@gmail.com>
2017-11-05 13:45:04 -0500

    final cut

--- src/capitalOne.js
+++ src/capitalOne.js
@@ -1,16 +1,13 @@
 const Wreck = require('wreck');
 const LuCategory = require('./cat_lookup');
 const _ = require('lodash')
+const Moment = require('moment');
 
-
-const BUDGET = {};
-const CAPITAL_ID = 401930000;
+const BUDGET = { };
 const facebookToCapitalId  = (id) => {
   let map = {
-     1732853743453321:401930000
-    ,142169726418680:401930000
+    1484444364926018:401930000
   }
-
   return map[id] || 401950000
 }
 
@@ -29,6 +26,59 @@ const API_URL = 'https://3hkaob4gkc.execute-api.us-east-1.amazonaws.com/prod/au-
     if (!customerId) return null;
     BUDGET[customerId] = amount;
   }
+
+  /**
+   *  doesn't work
+   */
+  listRewards = (fbid, month) => {
+    let customerId = facebookToCapitalId(sender_id);
+    return Wreck.post(API_URL+'customers/',
+    {payload: {
+      "customer_id": customerId
+      ,"team_name": "ed_and_roland"
+    }}).then(res => {
+      let resArray = JSON.parse(res.payload.toString())[0];
+      let accountId = resArray.customers[0].account_id;
+      return Wreck.post(API_URL + 'rewards/', {payload: {
+        "account_id": accountId  
+        ,"date_from": month + "/01/2017"
+        ,"date_to": (month+1)+"/01/2017"
+      }})  
+    })
+  }  
+
+  getMyself  = (sender_id) => {
+    let customerId = facebookToCapitalId(sender_id);
+    return Wreck.post(API_URL+'customers/',
+    {payload: {
+      "customer_id": customerId
+      ,"team_name": "ed_and_roland"
+    }}).then(res => {
+      let resArray = JSON.parse(res.payload.toString())[0];
+      let allowance = BUDGET[customerId] || 'Allowance not set';  
+      return { title: 'yourself', subtitle: allowance, id: resArray.customers[0].customer_id  } ;
+    })
+  }
+
+  getUnrestrictedMembers = (sender_id) => {
+    return listFamilyMembers(sender_id).then(res => {
+      return res.filter(it => it.subtitle.includes('not set')) 
+    })
+  }
+
+  getIdByName = (sender_id, name) => {
+    return listFamilyMembers(sender_id).then(family => {
+      let person = family.find(it => it.title.includes(name))
+      if (person) return  person.id 
+      else return Promise.resolve(undefined);
+    }).then(id => {
+      month = Moment().month(); 
+      return listTransactionsMonth(id,month).then(done =>{
+        return id;
+      })
+    })
+  }
+
 
   listFamilyMembers=(fbid) => {
     let customerId = facebookToCapitalId(fbid);
@@ -51,8 +101,9 @@ const API_URL = 'https://3hkaob4gkc.execute-api.us-east-1.amazonaws.com/prod/au-
       let promises =  familyIds.map(id => Wreck.post(API_URL+'customers', {payload: {"customer_id":id} }).then(function(res) {
         let resArray = JSON.parse(res.payload.toString())[0];
         let name = resArray.customers[0].first_name;
-        let lname = resArray.customers[0].last_name;
-        return { title: name, subtitle: lname  } ;
+        let id = resArray.customers[0].customer_id; 
+        let allowance = (BUDGET[id])? ('$'+BUDGET[id]) : 'Allowance not set';  
+        return { title: name, subtitle: allowance, id: resArray.customers[0].customer_id  } ;
       }))
       return Promise.all(promises)
       })
@@ -64,8 +115,7 @@ const API_URL = 'https://3hkaob4gkc.execute-api.us-east-1.amazonaws.com/prod/au-
   }
 
   //expects month to be in mm format
-  listTransactionsMonth= (fbid, month) => {
-    let customerId = facebookToCapitalId(fbid);
+  listTransactionsMonth= (customerId, month) => {
     if (!customerId) return Promise.resolve(null);
     return Wreck.post(API_URL+'transactions/',
       {payload: {
@@ -83,6 +133,11 @@ const API_URL = 'https://3hkaob4gkc.execute-api.us-east-1.amazonaws.com/prod/au-
   }
   checkWithinBudget=(fbid,month) => {
     let budget = getBudget(fbid);
+    if (!budget) {
+      return Promise.resolve({
+        answer: false  
+      }) 
+    } 
     return listTransactionsMonth(fbid,month).then(sum => {
       let answer = (budget >= sum);
       return  {
@@ -97,9 +152,15 @@ const API_URL = 'https://3hkaob4gkc.execute-api.us-east-1.amazonaws.com/prod/au-
     setBudget(fbid, limit)
     return Promise.resolve();
   }
+  setAllowanceLimit = (custId, limit) => {
+    BUDGET[custId] = limit
+    return Promise.resolve(); 
+  }
 
 
-  listTransactionsByCategory = (fbid, month) => {
+
+
+  listTransactionsByCategory = (customerId, month) => {
 
     if (state['transactions']) {
       let stuff = state.transactions.reduce((acc,curr) => {
@@ -132,6 +193,12 @@ module.exports ={
   listTransactionsByCategory:listTransactionsByCategory
   ,listTransactionsMonth:listTransactionsMonth
   ,listFamilyMembers:listFamilyMembers
+  ,listRewards: listRewards
   ,setBudgetLimit:setBudgetLimit
+  ,setAllowanceLimit: setAllowanceLimit
   ,checkWithinBudget:checkWithinBudget
+  ,getUnrestrictedMembers: getUnrestrictedMembers
+  ,getMyself: getMyself
+  ,getIdByName: getIdByName
+  ,facebookToCapitalId:facebookToCapitalId
 };
--- src/server.js
+++ src/server.js
@@ -6,10 +6,11 @@ const CapitalOne = require('./capitalOne');
 const Moment = require('moment');
 const Cutler = require('./cutler')
 
-
-// LETS SAVE USER SESSIONS
-var sessions = {}
-
+let allowanceQuestion = false;
+let budgetQuestion = false;
+let allowanceMembers = []; 
+let allowanceId = undefined;
+let verb = '';
 
 const Server = new Hapi.Server();
 
@@ -54,12 +55,26 @@ Server.route({
 
                         if (msg.includes('spend') && msg.includes('month')) {
                             let month =  Moment().month();
-                            return CapitalOne.listTransactionsMonth(sender_id,month).then(amount => {
+                            let capitalId = CapitalOne.facebookToCapitalId(sender_id); 
+                            return CapitalOne.listTransactionsMonth(capitalId,month).then(amount => {
                                 return Cutler.talk(sender_id, 'You spent $' + amount +' this month.')
                             }) ;
                         }
 
-                        else if (msg.includes('budget') && msg.includes('month') &&!msg.includes('set')) {
+                        else if (msg.toLowerCase().includes('eddie') && !msg.toLowerCase().includes('lower')) {
+                            let month = Moment().month(); 
+                            CapitalOne.getIdByName(sender_id,'Eddie').then(id => {
+                                CapitalOne.listTransactionsByCategory(id).then(cats => {
+                                    Cutler.showList(sender_id, cats).then(done => {
+                                        Cutler.talk(sender_id, 'Eddie is over budget for Amusement and Enterainment.  (budget is $100 )');
+                                    });
+
+                                })
+                            })
+                        }
+
+                        else if (msg.includes('budget') && (msg.includes('month') ||  msg.includes('within')) &&!msg.includes('set')) {
+                            let month =  Moment().month();
                             CapitalOne.checkWithinBudget(sender_id,month).then(resp => {
                                 let answer = (resp.answer) ? 'You are within your budget of $' + resp.budget +
                                 '. You have spent $' + resp.sum + ' so far this month.'
@@ -75,39 +90,97 @@ Server.route({
                             })
                         }
 
+                        else if (msg.toLowerCase().includes('lower') && msg.toLowerCase().includes('eddie') 
+                             && msg.toLowerCase().includes('allowance')
+                        ) {
+                            let limit = msg.match(/\d/g).join(''); 
+                            CapitalOne.getIdByName(sender_id, 'Eddie').then(id => {
+                              CapitalOne.setAllowanceLimit(id,100-limit).then(done => {
+                                  Cutler.talk(sender_id, "OK, Eddie's limit has been set to " + (100-limit));
+                              })
+                            })
+                        }
+
                         else if ( (msg.includes('family') || msg.includes('members')) && (msg.includes('show') || msg.includes('list')) ) {
                             CapitalOne.listFamilyMembers(sender_id).then(fam => {
-                                  Cutler.showList(sender_id, fam.slice(1))
+                                  let newFam = fam.slice(1); 
+                                  Cutler.showList(sender_id, newFam).then(done => {
+                                      let notFound = fam.find(it => it.subtitle.includes('not set'))
+                                      if (notFound)  {
+                                          allowanceQuestion = true;
+                                          Cutler.talk(sender_id, 'Would you like to set up allowances?');
+                                      }
+
+                                  })
                             }, err => {
                                 console.log(err);
-                                Cutler.talk(sender_id, "INDIGESTION! TOO MUCH SOYLENT. Try again");
+                                Cutler.talk(sender_id, "I don't know how to respond to do that. Sorry.");
                             })
 
                         }
 
-                        else if (msg == 'What can I buy with my reward points?') {
+                        else if (msg.toLowerCase().includes('yes')) {
+                            if (allowanceQuestion) {
+                                allowanceQuestion = false;
+                                CapitalOne.getUnrestrictedMembers(sender_id).then(members => {
+                                    allowanceMembers = members;
+                                    let member = allowanceMembers.pop(); 
+                                    verb = 'Allowances'; 
+                                    allowanceId = member.id; 
+                                    Cutler.talk(sender_id, 'Enter allowance for ' + member.title); 
+                                    })
+                            }
+                            else if (budgetQuestion) {
+                                budgetQuestion = false;
+                                verb = 'Budget';
+                                CapitalOne.getMyself(sender_id).then(member =>{
+                                    allowanceId = member.id;
+                                    Cutler.talk(sender_id, 'Enter your budget for this month?');
+                                })
+                            }
+                            else {
+                                Cutler.talk(sender_id ,'OK');
+                            }
+                        }
+                        else if(allowanceId) {
+                            let amount = msg.match(/\d/g).join(''); 
+                            CapitalOne.setAllowanceLimit(allowanceId, amount).then(done => {
+                                let member = allowanceMembers.pop(); 
+                                if (member) {
+                                    allowanceId = member.id; 
+                                    Cutler.talk(sender_id, 'Enter allowance for ' + member.title); 
+                                } 
+                                else {
+                                  allowanceId = null; 
+                                  Cutler.talk(sender_id, verb + ' set.');
+                                }
+                            })
+                        }
+                        else if (allowanceQuestion) {
+                            allowanceQuestion = false;
+                            Cutler.talk(sender_id, 'OK.')
+                        }
+                        else if (msg.toLowerCase().includes('reward')) {
                             let month =  Moment().month();
-
-                              message = 'Your have have 12850 points to spend.'
-                                Cutler.talk(sender_id, message);
+                            CapitalOne.listRewards(sender_id).then(amount => {
+                              message = 'You have ' + amount + ' of points to spend.'
+                              Cutler.talk(sender_id, message);
+                            }) ;
                         }
 
-                        else if (msg == 'On What?') {
+                        else if (msg.toLowerCase().includes('categories') || msg.toLowerCase().includes('breakdown')) {
                             let month =  Moment().month();
                             CapitalOne.listTransactionsByCategory(sender_id, month).then(categories => {
                                 Cutler.showList(sender_id, categories);
                             });
                         }
-                        
                         else if (msg == "Lower Jimmy's allowance by $50 dollars.") {
                             let month =  Moment().month();
                             CapitalOne.getAllowance(sender_id).then(amount => {
-                                    CapitalOne.setAllowance(sender_id, amount-50).then(res => {
-
-                              message = "You have set Jimmy's allowance to " + (amount-50) + ' from ' + amount + "."
-
-                                Cutler.talk(sender_id, message);
-                            }) ;
+                               CapitalOne.setAllowance(sender_id, amount-50).then(res => {
+                                  message = "You have set Jimmy's allowance to " + (amount-50) + ' from ' + amount + "."
+                                  Cutler.talk(sender_id, message);
+                            });
                           })
                         }
 
